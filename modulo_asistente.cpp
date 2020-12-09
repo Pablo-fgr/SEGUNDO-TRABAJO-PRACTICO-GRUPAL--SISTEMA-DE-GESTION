7fe9568150f5a75d0800d8709e538352d5385f47
Pablo Gómez <68721411+Pablo-fgr@users.noreply.github.com>
2020-12-09 00:58:38 -0300

    Update modulo_asistente.cpp
    registros

--- modulo_asistente.cpp
+++ modulo_asistente.cpp
@@ -1,14 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+
+struct Fecha{
+	int Dia;
+	int Mes;
+	int Anio;
+};
+
+struct Usuarios{
+	char Usuario[10];
+	char Contrasenia[10];
+	char Apellido_y_Nombre[60]; 
+};
+
+struct Veterinario{
+	char Apellido_y_Nombre[60];
+	int Matricula;
+	int Dni;
+	char Telefono[25];
+};
+
+struct Mascota{
+	char Apellido_y_Nombre[60];
+	char Domicilio[60];
+	int DNI_duenio;
+	char Localidad[60];
+	Fecha Fecha_de_Nacimiento;
+	float Peso;
+	char Telefono[25];
+};
+
+struct Turnos{
+	int Matricula_de_veterinario;
+	Fecha fecha;
+	int DNI_duenio;
+	char Detalle_de_atencion[380];
+};
 
 void menu();
 
 int main()
 {
-    FILE *arch_usuarios;
-    FILE *arch_veterinarios;
-
-    arch_usuarios = fopen("Usuarios.dat", "a+b");
+    FILE *arch_usuarios = fopen("Usuarios.dat", "a+b");
+    FILE *arch_veterinarios = fopen("Veterinarios.dat", "a+b");
+    FILE *arch_mascotas = fopen ("Mascotas.dat", "a+b");
+    FILE *arch_turnos = fopen("Turnos.dat", "a+b");
+    
 
     menu();
 
